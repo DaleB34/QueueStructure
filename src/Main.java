0af052cff0cde4e99b626aa5f09d9ec246aefddf
DaleB34 <dalebolido@gmail.com>
2025-02-28 12:50:19 -0800

    made test output

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        Queue q = new Queue();
+        q.push(1);
+        q.push(2);
+        q.push(3);
+        q.push(4);
+        q.push(5);
+
+        q.showList();
     }
 }
